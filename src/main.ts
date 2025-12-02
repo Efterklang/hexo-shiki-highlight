608835034da4631dc1613f30fbbdc5e47c4d132a
Efterklang <gaojiaxing2004@qq.com>
2025-12-02 15:09:07 +0800

    refactor code, extract some function

--- src/main.ts
+++ src/main.ts
@@ -8,39 +8,6 @@ import { processConfig, REGISTERED_TRANSFORMERS } from "./config";
 import { join } from "path";
 import { transformerStyleToClass } from "@shikijs/transformers";
 
-function createShikiTools(lang: string, title: string, displayItems: any): string {
-  const leftSection = `<div class="left">
-    <div class="traffic-lights">
-      <span class="traffic-light red"></span>
-      <span class="traffic-light yellow"></span>
-      <span class="traffic-light green"></span>
-    </div>
-    ${displayItems.lang ? `<div class="code-lang">${lang.toUpperCase()}</div>` : ''}
-  </div>`;
-
-  const centerSection = `<div class="center">
-    ${displayItems.title && title ? `<div class="code-title">${title}</div>` : ''}
-  </div>`;
-
-  let rightButtons = '';
-  if (displayItems.wrapToggle) {
-    rightButtons += '<iconify-icon icon="fa6-solid:arrow-down-wide-short" class="toggle-wrap" title="Toggle Wrap"></iconify-icon>';
-  }
-  if (displayItems.copyButton) {
-    rightButtons += '<div class="copy-notice"></div><iconify-icon icon="fa6-solid:paste" class="copy-button"></iconify-icon>';
-  }
-  if (displayItems.shrinkButton !== undefined) {
-    const closedClass = displayItems.shrinkButton === false ? ' closed' : '';
-    rightButtons += `<iconify-icon icon="fa6-solid:angle-down" class="expand${closedClass}"></iconify-icon>`;
-  }
-
-  const rightSection = `<div class="right">${rightButtons}</div>`;
-
-  const closedClass = displayItems.shrinkButton === false ? ' closed' : '';
-  return `<div class="shiki-tools${closedClass}">${leftSection}${centerSection}${rightSection}</div>`;
-};
-
-
 export async function init(hexo: Hexo): Promise<void> {
   const {
     themes, excludes, aliases, enableTransformers, styleToClass, toolbarItems,
@@ -54,29 +21,7 @@ export async function init(hexo: Hexo): Promise<void> {
   });
 
   (global as any).hexo = hexo;
-
-  if (customCSS) {
-    hexo.extend.injector.register("head_end", () => {
-      return `<link rel="stylesheet" href="${hexo.config.root}${customCSS}">`;
-    });
-  } else {
-    hexo.extend.generator.register("shiki_custom_css", () => {
-      return {
-        path: "css/code_block/shiki.css",
-        data: () => readFileSync(join(__dirname, "../code_block/shiki.css"), "utf-8"),
-      }
-    });
-    hexo.extend.injector.register("head_end", () => `<link rel="stylesheet" href="${hexo.config.root}css/code_block/shiki.css">`);
-  }
-
-  hexo.extend.generator.register("shiki_assets", () => {
-    return {
-      path: "js/code_block/shiki.js",
-      data: () => createReadStream(join(__dirname, "../code_block/shiki.js")),
-    };
-  });
-  hexo.extend.injector.register("body_end", () => `<script defer src="${hexo.config.root}js/code_block/shiki.js"></script>`);
-
+  hexoExtend(customCSS);
   const toClass = transformerStyleToClass({ classPrefix: styleToClass.classPrefix });
   const hexo_highlighter = (code: string, options: HighlightOptions) => {
     if (excludes.includes(options.lang || "")) {
@@ -125,3 +70,59 @@ export async function init(hexo: Hexo): Promise<void> {
 
   hexo.extend.highlight.register("shiki", hexo_highlighter);
 }
+
+function hexoExtend(customCSS: string | undefined) {
+  if (customCSS) {
+    hexo.extend.injector.register("head_end", () => {
+      return `<link rel="stylesheet" href="${hexo.config.root}${customCSS}">`;
+    });
+  } else {
+    hexo.extend.generator.register("shiki_custom_css", () => {
+      return {
+        path: "css/code_block/shiki.css",
+        data: () => readFileSync(join(__dirname, "../code_block/shiki.css"), "utf-8"),
+      }
+    });
+    hexo.extend.injector.register("head_end", () => `<link rel="stylesheet" href="${hexo.config.root}css/code_block/shiki.css">`);
+  }
+
+  hexo.extend.generator.register("shiki_assets", () => {
+    return {
+      path: "js/code_block/shiki.js",
+      data: () => createReadStream(join(__dirname, "../code_block/shiki.js")),
+    };
+  });
+  hexo.extend.injector.register("body_end", () => `<script defer src="${hexo.config.root}js/code_block/shiki.js"></script>`);
+}
+
+function createShikiTools(lang: string, title: string, displayItems: any): string {
+  const leftSection = `<div class="left">
+    <div class="traffic-lights">
+      <span class="traffic-light red"></span>
+      <span class="traffic-light yellow"></span>
+      <span class="traffic-light green"></span>
+    </div>
+    ${displayItems.lang ? `<div class="code-lang">${lang.toUpperCase()}</div>` : ''}
+  </div>`;
+
+  const centerSection = `<div class="center">
+    ${displayItems.title && title ? `<div class="code-title">${title}</div>` : ''}
+  </div>`;
+
+  let rightButtons = '';
+  if (displayItems.wrapToggle) {
+    rightButtons += '<iconify-icon icon="fa6-solid:arrow-down-wide-short" class="toggle-wrap" title="Toggle Wrap"></iconify-icon>';
+  }
+  if (displayItems.copyButton) {
+    rightButtons += '<div class="copy-notice"></div><iconify-icon icon="fa6-solid:paste" class="copy-button"></iconify-icon>';
+  }
+  if (displayItems.shrinkButton !== undefined) {
+    const closedClass = displayItems.shrinkButton === false ? ' closed' : '';
+    rightButtons += `<iconify-icon icon="fa6-solid:angle-down" class="expand${closedClass}"></iconify-icon>`;
+  }
+
+  const rightSection = `<div class="right">${rightButtons}</div>`;
+
+  const closedClass = displayItems.shrinkButton === false ? ' closed' : '';
+  return `<div class="shiki-tools${closedClass}">${leftSection}${centerSection}${rightSection}</div>`;
+};
